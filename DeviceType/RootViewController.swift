1e4630c1412127a2f50e2f9666949f93f06316d6
Alex <therebeloper@gmail.com>
2019-02-26 13:10:11 +0200

    first commit

--- DeviceType/RootViewController.swift
+++ DeviceType/RootViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import TinyConstraints
+import Device
 
 class RootViewController: UIViewController {
+    
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "Lorem ipsum dolor sit amet, consectetur adipisci"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: Device.size() == Size.screen4Inch ? 12 : 16)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .white
+        view.addSubview(label)
+        label.topToSuperview(offset: 36, usingSafeArea: true)
+        label.leftToSuperview(offset: 12, usingSafeArea: true)
+        label.rightToSuperview(offset: -12, usingSafeArea: true)
+        label.height(40)
     }
 
 
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+  platform :ios, '12.0'
+
+target 'DeviceType' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for DeviceType
+    pod 'TinyConstraints'
+    pod 'Device'
+
+end
